640c869b90585704b31f13e8c5ff81e066647ae6
Ahmed164-hash <muhammad.ahmed.raza164@gmail.com>
2025-12-23 22:52:48 +0500

    Table using Loop
    Table using Loop

---
+++ app.js
@@ -0,0 +1,11 @@
+let tableNumber = Number(prompt("Enter any Number:"))
+
+if( !isNaN(tableNumber)){
+    for(let i = 0; i < 20; i++){
+
+        let number = i + 1
+        console.log(tableNumber + "X" + number + "=" + number * tableNumber)
+    }
+}else{
+    alert("Enter correct Number Please!")
+}
